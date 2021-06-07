72f366b453819c9d820067c769e23cc243e20dbc
Mathias Kegelmann <mathias.kegelmann@gmail.com>
2021-06-08 01:36:30 +0200

    Add my löocal README.mk

---
+++ README.mk
@@ -0,0 +1,5 @@
+Changes made to run this "now" and on Mac minikube:
+- Change helm repo from
+  https://kubernetes-charts.storage.googleapis.com/
+  to https://charts.helm.sh/stable
+- run "minikube tunnel" to be able to access Ingress
